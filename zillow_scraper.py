9ce799a443e698f2d0e35691104b40703aca9d50
Ehsan U <Ehsan-U>
2023-01-18 17:04:29 +0500

    DB added

--- zillow_scraper.py
+++ zillow_scraper.py
@@ -18,11 +18,6 @@ Base = declarative_base()
 """ Zillow """
 class Zillow_Scraper():
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-        'Accept': '*/*',
-        'host': 'www.zillow.com',
-    }
     sleep_range = [1.0 ,5.0]
     previous_agent = ''
     ID = 1
